08f310aa19073448a95d22cb0c723d26ef7fd659
Mitchell Long <mwlong23@gmail.com>
2017-08-25 12:54:43 -0700

    allow a single pizza to be created and shown below the form input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,64 @@
+//Business Logic
+function Pizza(size, sauce, toppings)  {
+  this.size = size;
+  this.sauce = sauce;
+  this.toppings = [toppings];
+};
+
+Pizza.prototype.priceCalculator = function() {
+
+  var sizePrice = 0;
+  var toppingsPrice = 0;
+  var saucePrice = 0;
+  if(this.size === "personal"){
+    sizePrice = 10.0;
+  }else if(this.size === "small"){
+    sizePrice = 12.0;
+  }else if(this.size === "medium"){
+    sizePrice = 14.0;
+  }else if(this.size=== "large"){
+    sizePrice = 18.0;
+  }else if(this.size === "extra"){
+    sizePrice = 20;
+  }else if( this.size ==="party"){
+    sizePrice =26;
+  }
+// debugger;
+  if(this.toppings[0].length > 2){
+    toppingsPrice = (this.toppings[0].length - 2) * 2.75;
+  }
+
+  if(this.sauce === "pesto" || this.sauce === "white"){
+    saucePrice = 3.0;
+  };
+  return toppingsPrice + saucePrice + sizePrice
+};
+
+//User Interface
 $(document).ready(function(){
-  $("#add-topping").click(function(event){
-    event.preventDefault();
-    // debugger;
-    $("#new-toppings").append('<div class="new-topping extra" >'+
-      '<div class="form group">'+
-        '<label for="topping">Select A Topping:</label>'+
-        '<select id="topping">'+
-          '<option value=10>Pepperoni</option>'+
-          '<option value=12>Sausage</option>'+
-          '<option value=14>Vegan Sausage</option>'+
-          '<option value=16>Red Onions</option>'+
-          '<option value=18>Black Olives</option>'+
-          '<option value=20>Green Peppers</option>'+
-          '<option value=22>Pineapple</option>'+
-          '<option value=22>Jalapenos</option>'+
-        '</select>'+
-      '</div>'+
-    '</div>');
-  });
-  $("form#new-pizza").submit(function(event){
+  // $(function(event){
+  //   event.preventDefault();
+  //   // debugger;
+  //   });
+  $("#add-pizza").click(function(event){
     event.preventDefault();
 
     var inputtedPizzaSize = $("#pizza-size").val();
     var inputtedSauce = $("#sauce").val();
-    var inputtedTopping = $("#topping").val();
+    var inputtedToppings = []
+    $("input:checkbox[name=topping]:checked").each(function(){
+      var selectedTopping = $(this).val();
+      inputtedToppings.push(selectedTopping);
+    });
+
+    var newPizza = new Pizza(inputtedPizzaSize,inputtedSauce,inputtedToppings);
+
+
+
+    $("#single-pizza-summary").append(newPizza.priceCalculator()                                      +newPizza.size + newPizza.sause
+                          +'<p><span class="order-field">Toppings:</span><br>'
+                          +newPizza.toppings + '</p>');
+
 
 
   });
