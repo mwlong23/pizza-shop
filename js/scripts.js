ad4f5f0966c131c5d67158501283e599e87ecd62
Mitchell Long <mwlong23@gmail.com>
2017-08-25 14:00:46 -0700

    clear toppings checkboxes when another pizza is added.

--- js/scripts.js
+++ js/scripts.js
@@ -6,37 +6,59 @@ function Pizza(size, sauce, toppings)  {
 };
 
 Pizza.prototype.priceCalculator = function() {
+if(this.size === "empty" || this.sauce=== "empty"){
+  alert("Please Make sure every field is complete!");
+  } else{
+    var sizePrice = 0;
+    var toppingsPrice = 0;
+    var saucePrice = 0;
+    if(this.size === "personal"){
+      sizePrice = 10.0;
+    }else if(this.size === "small"){
+      sizePrice = 12.0;
+    }else if(this.size === "medium"){
+      sizePrice = 14.0;
+    }else if(this.size=== "large"){
+      sizePrice = 18.0;
+    }else if(this.size === "extra"){
+      sizePrice = 20;
+    }else if( this.size ==="party"){
+      sizePrice =26;
+    }
+  // debugger;
+    if(this.toppings[0].length > 2){
+      toppingsPrice = (this.toppings[0].length - 2) * 2.75;
+    }
 
-  var sizePrice = 0;
-  var toppingsPrice = 0;
-  var saucePrice = 0;
-  if(this.size === "personal"){
-    sizePrice = 10.0;
-  }else if(this.size === "small"){
-    sizePrice = 12.0;
-  }else if(this.size === "medium"){
-    sizePrice = 14.0;
-  }else if(this.size=== "large"){
-    sizePrice = 18.0;
-  }else if(this.size === "extra"){
-    sizePrice = 20;
-  }else if( this.size ==="party"){
-    sizePrice =26;
+    if(this.sauce === "pesto" || this.sauce === "alfredo"){
+      saucePrice = 3.0;
+    };
+    var orderTotal = toppingsPrice + saucePrice + sizePrice;
+    return orderTotal;
   }
-// debugger;
-  if(this.toppings[0].length > 2){
-    toppingsPrice = (this.toppings[0].length - 2) * 2.75;
-  }
-
-  if(this.sauce === "pesto" || this.sauce === "white"){
-    saucePrice = 3.0;
-  };
-  var orderTotal = toppingsPrice + saucePrice + sizePrice;
-  return ordertotal;
 };
+function taxCalculator (subTotal){
+  taxRate = .0125;
+  total = subTotal+(subTotal*taxRate);
+  return total;
+}
+
+function clearForm() // THE CLEAR FORM FUNCTION
+{
+  $('input[type="checkbox"]').prop('checked', false); // Uncheck
+  $('input[type="checkbox"]').prop('checked', false); // Uncheck
+  $('input[type="checkbox"]').prop('checked', false); // Uncheck
+  $('input[type="checkbox"]').prop('checked', false); // Uncheck
+  $('input[type="checkbox"]').prop('checked', false); // Uncheck
+  $('input[type="checkbox"]').prop('checked', false); // Uncheck
+
+
+}
+
 
 //User Interface
 $(document).ready(function(){
+  subTotal = 0;
 
   $("#add-pizza").click(function(event){
     event.preventDefault();
@@ -47,13 +69,13 @@ $(document).ready(function(){
     $("input:checkbox[name=topping]:checked").each(function(){
       var selectedTopping = $(this).val();
       inputtedToppings.push(selectedTopping);
+      $('input[type="checkbox"]').prop('checked', false); // Uncheck
     });
 
     var newPizza = new Pizza(inputtedPizzaSize,inputtedSauce,inputtedToppings);
+    subTotal += newPizza.priceCalculator();
 
-
-
-    $("#single-pizza-summary").append(newPizza.priceCalculator()                                      +newPizza.size + newPizza.sause
+    $("#single-pizza-summary").append(subTotal + taxCalculator(newPizza.priceCalculator())                                      +newPizza.size + newPizza.sauce
                           +'<p><span class="order-field">Toppings:</span><br>'
                           +newPizza.toppings + '</p>');
 
